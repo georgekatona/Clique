6f0f2019f0199815c8e6bfad7775d3140662b975
György Ádám Katona <a11723821@unet.univie.ac.at>
2018-11-09 10:07:31 +0100

    Add accessibility for command line

--- Clique.py
+++ Clique.py
@@ -1,9 +1,11 @@
 import os
+import sys
+
 import numpy as np
 import scipy.sparse.csgraph
 
 
-def insert_if_join__condition(candidates, item, item2, current_dim):
+def insert_if_join_condition(candidates, item, item2, current_dim):
     joined = item + item2
 
     dims = set()
@@ -27,11 +29,11 @@ def self_join(prev_dim_dense_units, dim):
     candidates = []
     for i in range(len(prev_dim_dense_units)):
         for j in range(i + 1, len(prev_dim_dense_units)):
-            insert_if_join__condition(candidates, prev_dim_dense_units[i], prev_dim_dense_units[j], dim)
+            insert_if_join_condition(candidates, prev_dim_dense_units[i], prev_dim_dense_units[j], dim)
     return candidates
 
 
-def is_data_in_projection(tuple, candidate):
+def is_data_in_projection(tuple, candidate, xsi):
     # print("tuple: ", tuple)
     # print("candidate: ", candidate)
     for dim in candidate:
@@ -41,21 +43,22 @@ def is_data_in_projection(tuple, candidate):
     return True
 
 
-def get_dense_units_for_dim(prevDimDenseUnits, dim):
+def get_dense_units_for_dim(data, prev_dim_dense_units, dim, xsi, tau):
     print("get_next_dense_units()")
-    print("\tprev dim dense units: ", prevDimDenseUnits)
-    candidates = self_join(prevDimDenseUnits, dim)
-    prune(candidates, prevDimDenseUnits)
+    print("\tprev dim dense units: ", prev_dim_dense_units)
+    number_of_data_points = np.shape(data)[0]
+    candidates = self_join(prev_dim_dense_units, dim)
+    prune(candidates, prev_dim_dense_units)
     print("\tcandidates: ", candidates)
     projection = np.zeros(len(candidates))
-    for dataIndex in range(dataSize):
+    for dataIndex in range(number_of_data_points):
         for i in range(len(candidates)):
-            if is_data_in_projection(X[dataIndex], candidates[i]):
+            if is_data_in_projection(data[dataIndex], candidates[i], xsi):
                 projection[i] += 1
     print("\tprojection: ", projection)
-    isDense = projection > tau * dataSize
-    print("\tisDense: ", isDense)
-    return np.array(candidates)[isDense]
+    is_dense = projection > tau * number_of_data_points
+    print("\tis_dense: ", is_dense, "\n")
+    return np.array(candidates)[is_dense]
 
 
 def get_edge(node1, node2):
@@ -71,72 +74,91 @@ def get_edge(node1, node2):
     return 1
 
 
-def get_graph_from_dense_units(denseUnits):
+def build_graph_from_dense_units(dense_units):
     print("get_graph_from_dense_units()")
-    print("\tDense units:\n", denseUnits)
-    # print("\tShape: ", np.shape(denseUnits))
-    graph = np.identity(len(denseUnits))
+    print("\tDense units:\n", dense_units)
+    # print("\tShape: ", np.shape(dense_units))
+    graph = np.identity(len(dense_units))
 
-    for i in range(len(denseUnits)):
-        for j in range(len(denseUnits)):
-            graph[i, j] = get_edge(denseUnits[i], denseUnits[j])
+    for i in range(len(dense_units)):
+        for j in range(len(dense_units)):
+            graph[i, j] = get_edge(dense_units[i], dense_units[j])
 
-    print("\tGraph:\n", graph)
+    print("\tGraph:\n", graph, "\n")
     return graph
 
 
-# Read in Data with labels
-path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "mouse.csv")
-X = np.genfromtxt(path, dtype=float, delimiter=' ', usecols=[0, 1])
-y = np.genfromtxt(path, dtype="U10", delimiter=' ', usecols=[2])
+def run_clique(file_name, feature_columns, label_column, xsi, tau):
+    print("Running CLIQUE algorithm on " + file_name + " dataset, feature columns = " +
+          str(feature_columns) + ", label column = " + str(label_column) + ", xsi = " +
+          str(xsi) + ", tau = " + str(tau) + "\n")
 
-# Normalize each dimension to the [0,1] range
-numberOfFeatures = np.shape(X)[1]
-dataSize = np.shape(X)[0]
+    # Read in data with labels
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
+    data = np.genfromtxt(path, dtype=float, delimiter=' ', usecols=feature_columns)
+    y = np.genfromtxt(path, dtype="U10", delimiter=' ', usecols=[label_column])
 
-for f in range(numberOfFeatures):
-    X[:, f] -= min(X[:, f])
-    X[:, f] *= 1 / max(X[:, f])
+    number_of_features = np.shape(data)[1]
+    number_of_data_points = np.shape(data)[0]
 
-# Inputs of the algorithm
-xsi = 3
-tau = .3
+    # Normalize each dimension to the [0,1] range
+    normalize_features(data, number_of_features)
 
-# Finding 1 dimensional dense units
-projection = np.zeros((xsi, numberOfFeatures))
-for f in range(numberOfFeatures):
-    for element in X[:, f]:
-        projection[int(element * xsi % xsi), f] += 1
-print("1D projection:\n", projection)
+    # Finding 1 dimensional dense units
+    projection = np.zeros((xsi, number_of_features))
+    for f in range(number_of_features):
+        for element in data[:, f]:
+            projection[int(element * xsi % xsi), f] += 1
+    print("1D projection:\n", projection, "\n")
 
-isDense = projection > tau * dataSize
-print("isDense:\n", isDense)
+    is_dense = projection > tau * number_of_data_points
+    print("is_dense:\n", is_dense, "\n")
 
-oneDimDenseUnits = []
+    one_dim_dense_units = []
 
-for f in range(numberOfFeatures):
-    for unit in range(xsi):
-        if isDense[unit, f]:
-            oneDimDenseUnits.append([[f, unit]])
+    for f in range(number_of_features):
+        for unit in range(xsi):
+            if is_dense[unit, f]:
+                one_dim_dense_units.append([[f, unit]])
 
-# Getting 1 dimensional clusters
-graph = get_graph_from_dense_units(oneDimDenseUnits)
-clusters = scipy.sparse.csgraph.connected_components(graph, directed=False)
-print("Clusters:\n", clusters)
-
-# Finding 1 dimensional dense units
-currentDim = 2
-denseUnits = get_dense_units_for_dim(oneDimDenseUnits, 2)
-# Getting 2 dimensional clusters
-graph = get_graph_from_dense_units(denseUnits)
-clusters = scipy.sparse.csgraph.connected_components(graph, directed=False)
-print("Clusters:\n", clusters)
-
-# Finding dense units and clusters for dimension > 2
-currentDim += 1
-while currentDim <= numberOfFeatures & len(denseUnits) > 0:
-    denseUnits = get_dense_units_for_dim(denseUnits, currentDim)
-    graph = get_graph_from_dense_units(denseUnits)
+    # Getting 1 dimensional clusters
+    graph = build_graph_from_dense_units(one_dim_dense_units)
     clusters = scipy.sparse.csgraph.connected_components(graph, directed=False)
-    print("Clusters:\n", clusters)
-    currentDim += 1
+    print("Clusters:\n", clusters, "\n")
+
+    # Finding 1 dimensional dense units
+    current_dim = 2
+    dense_units = get_dense_units_for_dim(data, one_dim_dense_units, 2, xsi, tau)
+    # Getting 2 dimensional clusters
+    graph = build_graph_from_dense_units(dense_units)
+    clusters = scipy.sparse.csgraph.connected_components(graph, directed=False)
+    print("Clusters:\n", clusters, "\n")
+
+    # Finding dense units and clusters for dimension > 2
+    current_dim += 1
+    while current_dim <= number_of_features & len(dense_units) > 0:
+        dense_units = get_dense_units_for_dim(data, dense_units, current_dim, xsi, tau)
+        graph = build_graph_from_dense_units(dense_units)
+        clusters = scipy.sparse.csgraph.connected_components(graph, directed=False)
+        print("Clusters:\n", clusters)
+        current_dim += 1
+
+
+def normalize_features(data, number_of_features):
+    for f in range(number_of_features):
+        data[:, f] -= min(data[:, f])
+        data[:, f] *= 1 / max(data[:, f])
+
+
+if __name__ == "__main__":
+    # Sample run: python Clique.py mouse.csv [0,1] 2 3 0.3
+    if len(sys.argv) > 4:
+        fileName = sys.argv[1]
+        feature_columns = map(int, sys.argv[2].strip('[]').split(','))
+        label_column = int(sys.argv[3])
+        xsi = int(sys.argv[4])
+        tau = float(sys.argv[5])
+        run_clique(fileName, feature_columns, label_column, xsi, tau)
+    else:
+        # Running with default parameters and data set
+        run_clique("mouse.csv", [0, 1], 2, 3, 0.3)
