185569318f8e7ea3c5a275d909336c1a359667e7
György Ádám Katona <a11723821@unet.univie.ac.at>
2018-11-09 17:36:00 +0100

    Format code, console output

--- Clique.py
+++ Clique.py
@@ -7,27 +7,30 @@ import scipy.sparse.csgraph
 from Cluster import Cluster
 
 
+# Inserts joined item into candidates list only if its dimensionality fits
 def insert_if_join_condition(candidates, item, item2, current_dim):
     joined = item + item2
 
+    # Count number of dimensions
     dims = set()
     for i in range(len(joined)):
         dims.add(int(joined[i][0]))
+
+    # Insert if it fits
     if len(dims) == current_dim:
         candidates.append(joined)
 
 
+# Prune all candidates, which has a (k-1) dimensional projection not in (k-1) dim dense units
 def prune(candidates, prev_dim_dense_units):
     for i in range(len(candidates)):
         for j in range(len(candidates[i])):
             if not prev_dim_dense_units.__contains__([candidates[i][j]]):
-                print(candidates[i], " pruned")
                 candidates.remove(candidates[i])
                 break
 
 
 def self_join(prev_dim_dense_units, dim):
-    print("\tselfjoin()")
     candidates = []
     for i in range(len(prev_dim_dense_units)):
         for j in range(i + 1, len(prev_dim_dense_units)):
@@ -46,21 +49,29 @@ def is_data_in_projection(tuple, candidate, xsi):
 
 
 def get_dense_units_for_dim(data, prev_dim_dense_units, dim, xsi, tau):
-    print("get_next_dense_units()")
-    print("\tprev dim dense units: ", prev_dim_dense_units)
-    number_of_data_points = np.shape(data)[0]
     candidates = self_join(prev_dim_dense_units, dim)
     prune(candidates, prev_dim_dense_units)
-    print("\tcandidates: ", candidates)
+    print("candidates: ", candidates)
+
+    # Count number of elements in candidates
     projection = np.zeros(len(candidates))
+    number_of_data_points = np.shape(data)[0]
     for dataIndex in range(number_of_data_points):
         for i in range(len(candidates)):
             if is_data_in_projection(data[dataIndex], candidates[i], xsi):
                 projection[i] += 1
-    print("\tprojection: ", projection)
+    print("projection: ", projection)
     is_dense = projection > tau * number_of_data_points
-    print("\tis_dense: ", is_dense, "\n")
+    print("is_dense: ", is_dense)
     return np.array(candidates)[is_dense]
+
+
+def build_graph_from_dense_units(dense_units):
+    graph = np.identity(len(dense_units))
+    for i in range(len(dense_units)):
+        for j in range(len(dense_units)):
+            graph[i, j] = get_edge(dense_units[i], dense_units[j])
+    return graph
 
 
 def get_edge(node1, node2):
@@ -76,50 +87,11 @@ def get_edge(node1, node2):
     return 1
 
 
-def build_graph_from_dense_units(dense_units):
-    graph = np.identity(len(dense_units))
-    for i in range(len(dense_units)):
-        for j in range(len(dense_units)):
-            graph[i, j] = get_edge(dense_units[i], dense_units[j])
-    return graph
-
-
-def save_to_file(clusters):
-    file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "clusters.txt"), encoding='utf-8', mode="w+")
+def save_to_file(clusters, file_name):
+    file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name), encoding='utf-8', mode="w+")
     for i, c in enumerate(clusters):
         file.write("Cluster " + str(i) + ":\n" + str(c))
     file.close()
-
-
-def run_clique(file_name, feature_columns, label_column, xsi, tau):
-    print("Running CLIQUE algorithm on " + file_name + " dataset, feature columns = " +
-          str(feature_columns) + ", label column = " + str(label_column) + ", xsi = " +
-          str(xsi) + ", tau = " + str(tau) + "\n")
-
-    # Read in data with labels
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
-    data = np.genfromtxt(path, dtype=float, delimiter=' ', usecols=feature_columns)
-    y = np.genfromtxt(path, dtype="U10", delimiter=' ', usecols=[label_column])
-
-    # Normalize each dimension to the [0,1] range
-    normalize_features(data)
-
-    # Finding 1 dimensional dense units
-    dense_units = get_one_dim_dense_units(data, tau, xsi)
-
-    # Getting 1 dimensional clusters
-    clusters = get_clusters(dense_units, data, xsi)
-
-    # Finding dense units and clusters for dimension > 2
-    current_dim = 2
-    number_of_features = np.shape(data)[1]
-    while current_dim <= number_of_features & len(dense_units) > 0:
-        dense_units = get_dense_units_for_dim(data, dense_units, current_dim, xsi, tau)
-        for cluster in get_clusters(dense_units, data, xsi):
-            clusters.append(cluster)
-        current_dim += 1
-
-    save_to_file(clusters)
 
 
 def get_cluster_data_points(data, cluster_dense_units, xsi):
@@ -133,6 +105,7 @@ def get_cluster_data_points(data, cluster_dense_units, xsi):
             feature_index = cluster_dense_units[i][j][0]
             range_index = cluster_dense_units[i][j][1]
             tmp_points = tmp_points[np.where(np.floor(tmp_points[:, feature_index] * xsi % xsi) == range_index)]
+        print(tmp_points)
         for element in tmp_points:
             cluster_points.append(element)
 
@@ -140,8 +113,6 @@ def get_cluster_data_points(data, cluster_dense_units, xsi):
 
 
 def get_clusters(dense_units, data, xsi):
-    print("get_clusters()")
-    print("dense_units:", dense_units)
     graph = build_graph_from_dense_units(dense_units)
     number_of_components, component_list = scipy.sparse.csgraph.connected_components(graph, directed=False)
 
@@ -151,6 +122,7 @@ def get_clusters(dense_units, data, xsi):
     for i in range(number_of_components):
         # Get dense units of the cluster
         cluster_dense_units = dense_units[np.where(component_list == i)]
+        print("cluster_dense_units: ", cluster_dense_units.tolist())
 
         # Get dimensions of the cluster
         dimensions = set()
@@ -175,7 +147,7 @@ def get_one_dim_dense_units(data, tau, xsi):
             projection[int(element * xsi % xsi), f] += 1
     print("1D projection:\n", projection, "\n")
     is_dense = projection > tau * number_of_data_points
-    print("is_dense:\n", is_dense, "\n")
+    print("is_dense:\n", is_dense)
     one_dim_dense_units = []
     for f in range(number_of_features):
         for unit in range(xsi):
@@ -191,15 +163,49 @@ def normalize_features(data):
         data[:, f] *= 1 / max(data[:, f])
 
 
+def run_clique(file_name, feature_columns, label_column, xsi, tau, output_file="clusters.txt"):
+    print("Running CLIQUE algorithm on " + file_name + " dataset, feature columns = " +
+          str(feature_columns) + ", label column = " + str(label_column) + ", xsi = " +
+          str(xsi) + ", tau = " + str(tau) + "\n")
+
+    # Read in data with labels
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
+    data = np.genfromtxt(path, dtype=float, delimiter=' ', usecols=feature_columns)
+    y = np.genfromtxt(path, dtype="U10", delimiter=' ', usecols=[label_column])
+
+    # Normalize each dimension to the [0,1] range
+    normalize_features(data)
+
+    # Finding 1 dimensional dense units
+    dense_units = get_one_dim_dense_units(data, tau, xsi)
+
+    # Getting 1 dimensional clusters
+    clusters = get_clusters(dense_units, data, xsi)
+
+    # Finding dense units and clusters for dimension > 2
+    current_dim = 2
+    number_of_features = np.shape(data)[1]
+    while current_dim <= number_of_features & len(dense_units) > 0:
+        print("\n", str(current_dim), " dimensional clusters:")
+        dense_units = get_dense_units_for_dim(data, dense_units, current_dim, xsi, tau)
+        for cluster in get_clusters(dense_units, data, xsi):
+            clusters.append(cluster)
+        current_dim += 1
+
+    save_to_file(clusters, output_file)
+    print("\nClusters exported to " + output_file)
+
+
 if __name__ == "__main__":
     # Sample run: python Clique.py mouse.csv [0,1] 2 3 0.3
-    if len(sys.argv) > 4:
-        fileName = sys.argv[1]
-        feature_columns = map(int, sys.argv[2].strip('[]').split(','))
-        label_column = int(sys.argv[3])
-        xsi = int(sys.argv[4])
-        tau = float(sys.argv[5])
-        run_clique(fileName, feature_columns, label_column, xsi, tau)
+
+    if len(sys.argv) > 5:
+        run_clique(file_name=sys.argv[1], feature_columns=map(int, sys.argv[2].strip('[]').split(',')),
+                   label_column=int(sys.argv[3]), xsi=int(sys.argv[4]), tau=float(sys.argv[5]),
+                   output_file=sys.argv[5])
+    elif len(sys.argv) > 4:
+        run_clique(file_name=sys.argv[1], feature_columns=map(int, sys.argv[2].strip('[]').split(',')),
+                   label_column=int(sys.argv[3]), xsi=int(sys.argv[4]), tau=float(sys.argv[5]))
     else:
         # Running with default parameters and data set
         run_clique("mouse.csv", [0, 1], 2, 3, 0.1)
